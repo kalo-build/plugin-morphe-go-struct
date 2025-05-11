33fc903c35739913588e6110026434901ce05719
bnunamak <brennan@nunamaker.de>
2025-05-11 15:19:16 +0200

    feat: compile optionality (models, structs, enums, models + entities)

--- pkg/compile/compile.go
+++ pkg/compile/compile.go
@@ -1,6 +1,10 @@
 package compile
 
-import "github.com/kalo-build/morphe-go/pkg/registry"
+import (
+	"fmt"
+
+	"github.com/kalo-build/morphe-go/pkg/registry"
+)
 
 func MorpheToGo(config MorpheCompileConfig) error {
 	r, rErr := registry.LoadMorpheRegistry(config.RegistryHooks, config.MorpheLoadRegistryConfig)
@@ -8,44 +12,60 @@ func MorpheToGo(config MorpheCompileConfig) error {
 		return rErr
 	}
 
-	allEnumDefs, compileAllErr := AllMorpheEnumsToGoEnums(config, r)
-	if compileAllErr != nil {
-		return compileAllErr
+	hasEnums := r.HasEnums()
+	if hasEnums {
+		allEnumDefs, compileAllErr := AllMorpheEnumsToGoEnums(config, r)
+		if compileAllErr != nil {
+			return compileAllErr
+		}
+
+		_, writeEnumsErr := WriteAllEnumDefinitions(config, allEnumDefs)
+		if writeEnumsErr != nil {
+			return writeEnumsErr
+		}
 	}
 
-	_, writeEnumsErr := WriteAllEnumDefinitions(config, allEnumDefs)
-	if writeEnumsErr != nil {
-		return writeEnumsErr
+	hasModels := r.HasModels()
+	if hasModels {
+		allModelStructDefs, compileAllErr := AllMorpheModelsToGoStructs(config, r)
+		if compileAllErr != nil {
+			return compileAllErr
+		}
+
+		_, writeModelStructsErr := WriteAllModelStructDefinitions(config, allModelStructDefs)
+		if writeModelStructsErr != nil {
+			return writeModelStructsErr
+		}
 	}
 
-	allModelStructDefs, compileAllErr := AllMorpheModelsToGoStructs(config, r)
-	if compileAllErr != nil {
-		return compileAllErr
+	hasStructures := r.HasStructures()
+	if hasStructures {
+		allStructureStructDefs, compileAllErr := AllMorpheStructuresToGoStructs(config, r)
+		if compileAllErr != nil {
+			return compileAllErr
+		}
+
+		_, writeStructureStructsErr := WriteAllStructureStructDefinitions(config, allStructureStructDefs)
+		if writeStructureStructsErr != nil {
+			return writeStructureStructsErr
+		}
 	}
 
-	_, writeModelStructsErr := WriteAllModelStructDefinitions(config, allModelStructDefs)
-	if writeModelStructsErr != nil {
-		return writeModelStructsErr
-	}
+	hasEntities := r.HasEntities()
+	if hasEntities {
+		if !hasModels {
+			return fmt.Errorf("entities compilation requires models to be compiled")
+		}
 
-	allStructureStructDefs, compileAllErr := AllMorpheStructuresToGoStructs(config, r)
-	if compileAllErr != nil {
-		return compileAllErr
-	}
+		allEntityStructDefs, compileAllErr := AllMorpheEntitiesToGoStructs(config, r)
+		if compileAllErr != nil {
+			return compileAllErr
+		}
 
-	_, writeStructureStructsErr := WriteAllStructureStructDefinitions(config, allStructureStructDefs)
-	if writeStructureStructsErr != nil {
-		return writeStructureStructsErr
-	}
-
-	allEntityStructDefs, compileAllErr := AllMorpheEntitiesToGoStructs(config, r)
-	if compileAllErr != nil {
-		return compileAllErr
-	}
-
-	_, writeEntityStructsErr := WriteAllEntityStructDefinitions(config, allEntityStructDefs)
-	if writeEntityStructsErr != nil {
-		return writeEntityStructsErr
+		_, writeEntityStructsErr := WriteAllEntityStructDefinitions(config, allEntityStructDefs)
+		if writeEntityStructsErr != nil {
+			return writeEntityStructsErr
+		}
 	}
 
 	return nil
